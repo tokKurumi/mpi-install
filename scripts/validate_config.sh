498c330480728141ddec317cec401dc04fa97812
tokKurumi <tkurumi901@gmail.com>
2025-04-08 12:55:29 +0300

    refactor validate_config_content to accumulate errors and report them collectively

--- scripts/validate_config.sh
+++ scripts/validate_config.sh
@@ -28,45 +28,52 @@ validate_config_content() {
     local config_file=$1
     local required_master_fields=("username" "ip" "password" "cluster_name" "slurmctld_port" "slurmd_port")
     local required_slave_fields=("id" "username" "ip" "password")
+    local errors=()
 
     # Validate master node configuration
     for field in "${required_master_fields[@]}"; do
-        if ! jq -e ".master.${field}" "$config_file" >/dev/null; then
-            error "Master configuration is missing required field: $field"
-            exit 1
+        if ! jq -e ".master.${field}" "$config_file" >/dev/null 2>&1; then
+            errors+=("Master configuration is missing required field: $field")
         fi
     done
 
     # Validate slaves configuration
     local slave_count=$(jq '.slaves | length' "$config_file")
     if [[ "$slave_count" -eq 0 ]]; then
-        error "At least one slave node must be configured"
-        exit 1
-    fi
-
-    for ((i=0; i<slave_count; i++)); do
-        for field in "${required_slave_fields[@]}"; do
-            if ! jq -e ".slaves[${i}].${field}" "$config_file" >/dev/null; then
-                error "Slave $i is missing required field: $field"
-                exit 1
-            fi
+        errors+=("At least one slave node must be configured")
+    else
+        for ((i = 0; i < slave_count; i++)); do
+            for field in "${required_slave_fields[@]}"; do
+                if ! jq -e ".slaves[${i}].${field}" "$config_file" >/dev/null 2>&1; then
+                    errors+=("Slave $i is missing required field: $field")
+                fi
+            done
         done
-    done
+    fi
 
     # Validate IP addresses format
-    local master_ip=$(jq -r '.master.ip' "$config_file")
+    local master_ip=$(jq -r '.master.ip' "$config_file" 2>/dev/null)
     if ! [[ "$master_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        error "Master IP address $master_ip is invalid"
+        errors+=("Master IP address $master_ip is invalid")
+    fi
+
+    for ((i = 0; i < slave_count; i++)); do
+        local slave_ip=$(jq -r ".slaves[${i}].ip" "$config_file" 2>/dev/null)
+        if ! [[ "$slave_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            errors+=("Slave $i IP address $slave_ip is invalid")
+        fi
+    done
+
+    # Report all errors if any
+    if [[ ${#errors[@]} -gt 0 ]]; then
+        error "Found ${#errors[@]} configuration issues:"
+        for err in "${errors[@]}"; do
+            error "  - $err"
+        done
         exit 1
     fi
 
-    for ((i=0; i<slave_count; i++)); do
-        local slave_ip=$(jq -r ".slaves[${i}].ip" "$config_file")
-        if ! [[ "$slave_ip" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-            error "Slave $i IP address $slave_ip is invalid"
-            exit 1
-        fi
-    done
+    return 0
 }
 
 main() {
@@ -78,7 +85,7 @@ main() {
     local config_file=$1
 
     info "Validating configuration file: $config_file"
-    
+
     validate_config_file "$config_file"
     validate_config_content "$config_file"
 
