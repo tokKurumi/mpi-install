335c3912d12dcda968cbbe0ad6afbb1744938b51
tokKurumi <tkurumi901@gmail.com>
2025-04-10 13:50:58 +0300

    refactor run_setup.sh to explicitly invoke bash for script execution

--- run_setup.sh
+++ run_setup.sh
@@ -16,22 +16,22 @@ config_file=$1
 
 # Validate config first
 info "Validating configuration..."
-./scripts/validate_config.sh "$config_file"
+bash ./scripts/validate_config.sh "$config_file"
 
 # Setup SSH
 info "Setting up SSH keys..."
-./scripts/setup_ssh.sh "$config_file"
+bash ./scripts/setup_ssh.sh "$config_file"
 
 # Configure master
 info "Configurring master node..."
-./scripts/configure_master.sh "$config_file"
+bash ./scripts/configure_master.sh "$config_file"
 
 # Configure slaves
 info "Installing slave nodes..."
-./scripts/configure_slave.sh "$config_file"
+bash ./scripts/configure_slave.sh "$config_file"
 
 # Running & verifying services
 info "Running and verifying cluster services..."
-./scripts/run_services.sh "$config_file"
+bash ./scripts/run_services.sh "$config_file"
 
 success "Cluster setup completed successfully!"
